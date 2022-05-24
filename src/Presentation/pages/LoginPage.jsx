2bc9c040246dfc9ce0904a5ef3f0af67c32c6247
ZGennadiy <gennadiy.zinchenko@gmail.com>
2022-05-24 10:27:54 +0300

    layout and validation for login page

--- src/Presentation/pages/LoginPage.jsx
+++ src/Presentation/pages/LoginPage.jsx
@@ -1,66 +1,72 @@
-import { Formik } from 'formik';
-import React from 'react';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import React, { useCallback, useMemo } from 'react';
+import { Button } from 'react-bootstrap';
+import * as yup from 'yup';
+
+const validationSchema = yup.object({
+  nickName: yup
+    .string('Enter your nickName')
+    .min(2, 'Too Short!')
+    .required('Nick Name is required'),
+  password: yup
+    .string('Enter your password')
+    .min(6, 'Password should be of minimum 6 characters length')
+    .required('Password is required'),
+});
 
 function LoginPage() {
+const initialValues = useMemo(() => ({
+  nickName: '',
+  password: ''
+}), [])
+
+const onSubmit = useCallback((values) => {
+  console.log('!!!!', values);
+}, [])
+
   return (
-    <>
-      <div>
-        <h1>Anywhere in your app!</h1>
+    <div className="container-fluid h-100">
+      <div className="row justify-content-center align-content-center h-100">
+        <div className="col-12 col-md-8 col-xxl-6">
+          <div className="card shadow-sm p-4">
+            <h1>Sign in</h1>
+            <Formik
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+              onSubmit={onSubmit}
+            >
+                <Form className="d-flex flex-column align-item-center" >
+                <div className='d-flex flex-column mt-2'>
+                  <label htmlFor="nickName">Nick Name</label>
+                  <Field
+                    id="nickName"
+                    name="nickName"
+                    placeholder="JohnWick1974"
+                    className='form-control'
+                    autoFocus
+                    />
+                    <ErrorMessage component="p" name="nickName" className="invalid-feedback d-block" />
+                </div>
+                <div className='d-flex flex-column mt-2'>
+                  <label htmlFor="password">Password</label>
+                  <Field 
+                    id="password"
+                    type="password"
+                    name="password"
+                    className='form-control'
+                    />
+                    <ErrorMessage component="p" name="password" className="invalid-feedback d-block" />
+                </div>
+                <Button variant="outline-primary" className='mt-4 mb-4' type="submit">Sign in</Button>
+              </Form>
+              
+              
+            </Formik>
+          </div>
+        </div>
       </div>
-      <Formik
-        initialValues={{ email: '', password: '' }}
-        validate={(values) => {
-          const errors = {};
-          if (!values.email) {
-            errors.email = 'Required';
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = 'Invalid email address';
-          }
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
-        }}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-          /* and other goodies */
-        }) => (
-          <form onSubmit={handleSubmit}>
-            <input
-              type="email"
-              name="email"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.email}
-            />
-            {errors.email && touched.email && errors.email}
-            <input
-              type="password"
-              name="password"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.password}
-            />
-            {errors.password && touched.password && errors.password}
-            <button type="submit" disabled={isSubmitting}>
-              Submit
-            </button>
-          </form>
-        )}
-      </Formik>
-    </>
+
+    </div>
   );
 }
 
